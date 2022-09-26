4b1e244de9d249b9cd61d0ba46c9523fa3506a8d
Usman Husnan <oets.man@gmail.com>
2022-09-26 12:35:25 +0700

    content selesai

---
+++ assets/scripts/script.js
@@ -0,0 +1,52 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+    mantul();
+  });
+});
+
+function mantul() {
+  setTimeout(function () {
+    var y = window.pageYOffset || document.documentElement.scrollTop;
+    if (window.innerWidth < 800) {
+      window.scrollTo({ top: y - 400, behavior: "smooth" });
+    } else {
+      window.scrollTo({ top: y - 200, behavior: "smooth" });
+    }
+  }, 500);
+}
+
+function hideShow() {
+  let elements = document.querySelectorAll(".list-menu");
+  // using forEach loop
+  elements.forEach((element) => {
+    if (element.style.display == "none") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+}
+
+document.querySelectorAll(".more").forEach((el) => {
+  el.addEventListener("click", () => {
+    const id = el.id.substring(el.id.length - 1);
+    showDetail(id);
+  });
+});
+
+function showDetail(number) {
+  const second = "a" + number;
+  const more = "more" + number;
+  const el = document.getElementById(second);
+  if (el.classList.contains("second")) {
+    el.classList.remove("second");
+    document.getElementById(more).innerText = "Sembunyikan...";
+  } else {
+    el.classList.add("second");
+    document.getElementById(more).innerText = "Selengkapnya...";
+  }
+}
